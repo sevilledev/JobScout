05d51905b5686c8f8a61faf67b6ed848fd1fb5c7
sevilledev <sevillerasulova@gmail.com>
2022-03-10 20:26:38 +0400

    procfile added

--- jobscout/jobscout/settings.py
+++ jobscout/jobscout/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,5 @@ BLOG_STORAGE = os.path.join(MEDIA_ROOT, "blog_media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
